70e7bb796ed585ce34a893cbd41a6963b2a68e87
HungNV <hungnv256196@gmail.com>
2017-09-12 20:10:12 +0700

    commit changed

---
+++ app/src/main/java/com/example/windows10gamer/beautimusic/view/helper/dragandswipe/ItemTouchHelperAdapter.java
@@ -0,0 +1,10 @@
+package com.example.windows10gamer.beautimusic.view.helper.dragandswipe;
+
+/**
+ * Created by Windows 10 Gamer on 12/09/2017.
+ */
+
+public interface ItemTouchHelperAdapter {
+    void onItemDismiss(int position);
+    boolean onItemMove(int fromPosition, int toPosition);
+}
---
+++ app/src/main/java/com/example/windows10gamer/beautimusic/view/helper/dragandswipe/ItemTouchHelperViewHolder.java
@@ -0,0 +1,14 @@
+package com.example.windows10gamer.beautimusic.view.helper.dragandswipe;
+
+/**
+ * Created by Windows 10 Gamer on 12/09/2017.
+ */
+
+public interface ItemTouchHelperViewHolder  {
+
+    void onItemSelected ();
+
+
+    void onItemClear ();
+
+}
